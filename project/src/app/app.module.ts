80a9178f16ccfeda15ca576fd5f28615961dacca
RodrigoEmmanoel <rodrigoemmanoel98@gmail.com>
2021-07-16 20:38:45 -0300

    feat(module/roating):  component connection routes, angular optimization modules, component functionality and export of the Pessoa class

---
+++ project/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListarComponent } from './Pessoa/listar/listar.component';
+import { AddComponent } from './Pessoa/add/add.component';
+import { EditComponent } from './Pessoa/edit/edit.component';
+import { FormsModule } from '@angular/forms';
+import { ServiceService } from '../app/Service/service.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent, ListarComponent, AddComponent, EditComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
